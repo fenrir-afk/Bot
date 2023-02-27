2884a77209928f6ef3717073015924e95eeaab5d
Stepan <fenrird@inbox.ru>
2023-02-27 18:44:04 +0300

    first commit

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Branch1+First commit+Second Commit+third commit");
+        System.out.println("Branch1+First commit+Second Commit+third commit");
     }
 }
